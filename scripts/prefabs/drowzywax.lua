be4e94c621927b07a3deed950d7292a64fbbca8c
Emiliano Cendali <emiliancendalifer@gmail.com>
2026-04-23 21:32:06 -0300

    Update drowzywax.lua

--- scripts/prefabs/drowzywax.lua
+++ scripts/prefabs/drowzywax.lua
@@ -36,7 +36,7 @@ end
 
 local function oneaten(inst, eater)
     if not eater:HasTag("waxxxer") then
-        -- Si el consumidor no tiene la etiqueta "waxxxer", no permitas que consuma el ítem.
+
         return false
     end
 
@@ -82,7 +82,7 @@ local function MainFunction()
     inst:AddComponent("edible")
     inst.components.edible.foodtype = FOODTYPE.GENERIC
     inst.components.edible.hungervalue = 101
-    inst.components.edible.healthvalue = 131 -- This line makes the item restore 8 health points when eaten.
+    inst.components.edible.healthvalue = 131 
 	inst.components.edible.sanityvalue = 31
     inst.components.edible:SetOnEatenFn(oneaten)
 
@@ -90,10 +90,10 @@ local function MainFunction()
     inst.components.stackable.maxsize = TUNING.STACK_SIZE_SMALLITEM
 	inst.components.stackable:SetStackSize(1)
 
-    -- Función cuando se guarda en el inventario
+
     inst.components.inventoryitem.onputininventoryfn = function(inst, player)
         local owner = inst.components.inventoryitem:GetGrandOwner()
-        -- Verifica si el propietario es un jugador y no "waxbert"
+
         if owner and owner:HasTag("player") and owner.prefab ~= "waxbert" then
             if owner.components.inventory then
                 inst:DoTaskInTime(0, function()
@@ -108,13 +108,13 @@ local function MainFunction()
 
     MakeHauntableLaunch(inst)
 
-    -- Tarea periódica para expulsar el item si no es "waxbert"
+
     inst:DoPeriodicTask(0.1, function(inst)
         local owner = inst.components.inventoryitem.owner
-        -- Solo ejecuta si el propietario es un jugador y no es "waxbert"
+
         if owner and owner:HasTag("player") and owner.prefab ~= "waxbert" then
             if owner.components.inventory then
-                -- Encuentra todos los items "whosphorus" en el inventario del personaje
+
                 local items = owner.components.inventory:FindItems(function(item) return item.prefab == "drowzywax" end)
                 for _, item in ipairs(items) do
                     owner.components.inventory:DropItem(item)
